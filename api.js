a33d592abe3e26c0143ec073b2123e7d485f8056
brunoobueno <brunobueno@id.uff.br>
2024-02-08 15:36:52 -0300

    importação de api

---
+++ api.js
@@ -0,0 +1,137 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
+const port = process.env.PORT || 3000; // Altere a porta conforme necessário
+
+// Caminhos para os arquivos .pem
+const SSL_CA_PATH = 'DigiCertGlobalRootCA.crt.pem'; // Substitua pelo caminho correto
+
+
+const dbConfig = {
+  host: 'sjobanco.mysql.database.azure.com',
+  user: 'sjobanco',
+  password: '91922010bB',
+  database: 'db_sjo',
+  ssl: {
+    ca: fs.readFileSync(SSL_CA_PATH),
+  }
+};
+
+app.use(express.json());
+app.use(cors());
+
+const pool = mysql.createPool(dbConfig);
+
+// Função para executar consultas no banco de dados
+async function executeQuery(sql, values = []) {
+  const connection = await pool.getConnection();
+  try {
+    const [results] = await connection.query(sql, values);
+    return results;
+  } catch (error) {
+    throw error;
+  } finally {
+    connection.release(); // Liberar a conexão de volta para o pool
+  }
+}
+
+// Rota para buscar todos os produtos
+app.get('/produtos', async (req, res) => {
+  try {
+    // Consulta ao banco de dados para buscar todos os produtos
+    const [rows] = await pool.query('SELECT * FROM produtos');
+    res.status(200).json(rows); // Retorne os dados dos produtos como JSON
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+    res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+});
+
+// Rota para buscar informações de Pagamento
+app.get('/pagamentos', async (req, res) => {
+  try {
+    // Consulta ao banco de dados para buscar informações de Pagamento
+    const [rows] = await pool.query('SELECT * FROM pagamentos');
+    res.status(200).json(rows); // Retorne os dados de pagamento como JSON
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+    res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+});
+
+// Rota para buscar informações da Empresa
+app.get('/empresa', async (req, res) => {
+  try {
+    // Consulta ao banco de dados para buscar informações da empresa
+    const [rows] = await pool.query('SELECT * FROM empresa');
+    res.status(200).json(rows); // Retorne os dados da empresa como JSON
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+    res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+});
+
+// Rota para buscar todos os saudacao
+app.get('/saudacao', async (req, res) => {
+  try {
+    // Consulta ao banco de dados para buscar todos os produtos
+    const [rows] = await pool.query('SELECT * FROM saudacao');
+    res.status(200).json(rows); // Retorne os dados dos produtos como JSON
+  } catch (error) {
+    console.error('Erro ao buscar saudacao:', error);
+    res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+});
+
+// Rota para buscar informações da troca
+app.get('/troca', async (req, res) => {
+  try {
+    // Consulta ao banco de dados para buscar informações da empresa
+    const [rows] = await pool.query('SELECT * FROM troca');
+    res.status(200).json(rows); // Retorne os dados da empresa como JSON
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+    res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+});
+
+// Rota para buscar informações da  promocao
+app.get('/promocao', async (req, res) => {
+  try {
+    // Consulta ao banco de dados para buscar informações da empresa
+    const [rows] = await pool.query('SELECT * FROM promocao');
+    res.status(200).json(rows); // Retorne os dados da promocao como JSON
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+    res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+});
+
+app.get('/entrega', async (req, res) => {
+  try {
+    // Consulta ao banco de dados para buscar informações da empresa
+    const [rows] = await pool.query('SELECT * FROM entrega');
+    res.status(200).json(rows); // Retorne os dados da promocao como JSON
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+    res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+});
+
+app.get('/pedido', async (req, res) => {
+  try {
+    // Consulta ao banco de dados para buscar informações da empresa
+    const [rows] = await pool.query('SELECT * FROM pedido');
+    res.status(200).json(rows); // Retorne os dados da promocao como JSON
+  } catch (error) {
+    console.error('Erro ao buscar informações de compra:', error);
+    res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor API rodando na porta ${port}`);
+  });
+  
